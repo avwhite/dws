bcfbdc5bb04aaa1334bfc6d53bd9a15b02b2e0cd
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2020-07-15 19:11:46 +0200

    more tests and fixes (flange does not sound bugged anymore??)

--- src/delay_line.rs
+++ src/delay_line.rs
@@ -1,6 +1,7 @@
 use dasp::frame::Frame;
 use dasp_ring_buffer::*;
 use dasp::Sample;
+use approx::relative_eq;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct DelayLine<S> {
@@ -37,7 +38,7 @@ where
 
     /// Borrows the item at the given index relative to the input (0 is the last input value)
     pub fn tap(&self, index: usize) -> S::Element {
-        assert!(index + 1 < self.capacity());
+        assert!(index + 1 < self.data.slice().len());
 
         let wrapped_index: usize;
 
@@ -58,10 +59,19 @@ where
         }
     }
 
-    /// Borrows the item at the given index relative to the output (0 is the next value to be output)
-    /// we should have tap_output(n) == tap_input(delay + n)1
+    /// Borrows the item at the given index relative to the output (0 is previously output value)
     pub fn tap_output(&self, index: usize) -> S::Element {
-        self.tap(self.get_delay() + index)
+        assert!(index + 1 < self.data.slice().len());
+
+        let wrapped_index: usize;
+
+        if index + 1 > self.out_point {
+            wrapped_index = self.data.slice().len() - (index + 1 - self.out_point);
+        } else {
+            wrapped_index = self.out_point - (index + 1);
+        }
+
+        self.data.slice()[wrapped_index]
     }
 
     pub fn set_delay(&mut self, delay: usize) {
@@ -119,7 +129,14 @@ where
         let out_integer_part =
             out_integer.scale_amp((1.0 - self.fractional_delay_part).to_sample());
 
-        let out_frac = self.delay_line.tap_output(1);
+        let out_frac = if approx::relative_eq!(self.fractional_delay_part, 0.0) {
+            //if the delay is exactly equal to the maximum delay, we cannot tap at one past the output
+            //but in that case the fractional part is zero, so the fractional output does not matter anyway
+            //and we can just set it to anything
+            T::Element::EQUILIBRIUM 
+        } else {
+            self.delay_line.tap_output(1)
+        };
         let out_frac_part = out_frac.scale_amp(self.fractional_delay_part.to_sample());
 
         out_integer_part.add_amp(out_frac_part.to_signed_frame())
@@ -137,6 +154,8 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use approx::assert_relative_eq;
+    use rand::distributions::{Distribution, Uniform};
 
     #[test]
     pub fn zero_integer_delay() {
@@ -218,12 +237,14 @@ mod tests {
     pub fn integer_max_delay2() {
         let mut d = DelayLine::new(vec![0; 100], 95);
         d.set_delay(99);
+        d.tick(0);
     }
 
     #[test]
     #[should_panic]
     pub fn integer_delay_too_big1() {
-        DelayLine::new(vec![0; 100], 100);
+        let mut d = DelayLine::new(vec![0; 100], 100);
+        d.tick(0);
     }
 
     #[test]
@@ -231,5 +252,92 @@ mod tests {
     pub fn integer_delay_too_big2() {
         let mut d = DelayLine::new(vec![0; 100], 5);
         d.set_delay(100);
+    }
+
+    #[test]
+    pub fn zero_frac_delay() {
+        let mut d = DelayLineFracLin::new(vec![0; 100], 0.0);
+
+        for n in 0..1000 {
+            let v = d.tick(n);
+
+            assert_eq!(v, n);
+        }
+    }
+
+    #[test]
+    pub fn fixed_frac_delay() {
+        let delay = 9.4;
+
+        let mut d = DelayLineFracLin::new(vec![0.0; 100], delay);
+
+        for n in 0..12345 {
+            let float_n = n as f64;
+
+            let v = d.tick(float_n);
+
+            if float_n >= delay.ceil() {
+                //after the transient phase we expect values interpolated between values we previously put in
+                let out_a = float_n - delay.floor();
+                let out_b = float_n - delay.ceil();
+
+                let expected_out = out_a * (1.0 - delay.fract()) + out_b * delay.fract();
+
+                assert_relative_eq!(v, expected_out);
+            } else if float_n <= delay.floor() {
+                //At the beginning we should see zeroes
+                assert_relative_eq!(v, 0.0);
+            }
+            //In between these two cases we are interpolating between initial values and values we entered.
+            //not checking these for now.
+        }
+    }
+
+    #[test]
+    pub fn variable_frac_delay() {
+        let mut delay = 90.0;
+
+        let mut d = DelayLineFracLin::new(vec![0.0; 100], delay);
+        let udist = Uniform::new(0.0, 99.0);
+        let mut rng = rand::thread_rng();
+
+        for n in 0..12345 {
+            if n % 17 == 0 {
+                //change the delay sometimes
+                delay = udist.sample(&mut rng);
+                d.set_delay(delay);
+            }
+            let float_n = n as f64;
+
+            let v = d.tick(float_n);
+
+            if float_n >= delay.ceil() {
+                //after the transient phase we expect values interpolated between values we previously put in
+                let out_a = float_n - delay.floor();
+                let out_b = float_n - delay.ceil();
+
+                let expected_out = out_a * (1.0 - delay.fract()) + out_b * delay.fract();
+
+                assert_relative_eq!(v, expected_out);
+            } else if float_n <= delay.floor() {
+                //At the beginning we should see zeroes
+                assert_relative_eq!(v, 0.0);
+            }
+            //In between these two cases we are interpolating between initial values and values we entered.
+            //not checking these for now.
+        }
+    }
+
+    #[test]
+    pub fn frac_max_delay1() {
+        let mut d = DelayLineFracLin::new(vec![0; 100], 99.0);
+        d.tick(0);
+    }
+
+    #[test]
+    pub fn frac_max_delay2() {
+        let mut d = DelayLineFracLin::new(vec![0; 100], 95.0);
+        d.set_delay(99.0);
+        d.tick(0);
     }
 }
