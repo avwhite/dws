81e4f654d92db805f26cf2db4b5fe608e77b263a
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2020-07-16 15:09:48 +0200

    Be a vst plugin instead of a jack client.
    I think this is the best approach to start with since with vst I get a gui for changing parameters for free etc.
    I will probably do a jack client version later.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,23 @@
 pub mod delay_line;
 pub mod effects;
+
+#[macro_use]
+extern crate vst;
+
+use vst::plugin::{Info, Plugin};
+
+#[derive(Default)]
+struct BasicPlugin;
+
+impl Plugin for BasicPlugin {
+    fn get_info(&self) -> Info {
+        Info {
+            name: "dws".to_string(),
+            unique_id: 84781384, // Used by hosts to differentiate between plugins.
+
+            ..Default::default()
+        }
+    }
+}
+
+plugin_main!(BasicPlugin); // Important!
