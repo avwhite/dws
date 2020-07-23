b2f5cf37a7f8f0e1fe34493867061fb236dd7512
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2020-07-23 12:33:50 +0200

    Add FIR filter test

--- src/filter.rs
+++ src/filter.rs
@@ -24,6 +24,10 @@ where
         self.coefficients.as_mut_slice()
     }
 
+    pub fn get_coefficients(&self) -> &[f64] {
+        self.coefficients.as_slice()
+    }
+
     pub fn tick(&mut self, input: F) -> F {
         let mut output = input.scale_amp(self.coefficients[0].to_sample());
         for i in 1..self.coefficients.len() {
@@ -37,5 +41,24 @@ where
         }
         self.memory.tick(input);
         return output;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::filter::*;
+    #[test]
+    pub fn fir_impulse_response() {
+        //For a fir filter the impules response should equal the coefficients.
+
+        let coefs = vec![0.1, 0.2, 0.0, 2.1];
+        let mut filter = FIRFilter::<f64>::new(coefs.clone());
+
+        let mut impulse = vec![0.0; coefs.len()];
+        impulse[0] = 1.0;
+        for (c, i) in coefs.iter().zip(impulse) {
+            let o = filter.tick(i);
+            assert_eq!(*c, o);
+        }
     }
 }
